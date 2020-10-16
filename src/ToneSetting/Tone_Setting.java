b282971f26552da5faddf706ae7c5dfea82d551b
Preeti Roj <preetiroj2001@gmail.com>
2020-10-16 11:44:51 +0530

    tone setting bug fixed

--- src/ToneSetting/Tone_Setting.java
+++ src/ToneSetting/Tone_Setting.java
@@ -1,4 +1,4 @@
-package Menu;
+package ToneSetting;
 
 import javax.sound.sampled.*;
 import javax.swing.*;
@@ -17,7 +17,7 @@ public class Tone_Setting extends JFrame{
     private JButton playBT;
     private JButton stopBT;
     private JButton chooseBT;
-    private String file;
+    //private String file;
     public static Tone_Setting instance;
     private URL urlFile;
 
@@ -29,16 +29,19 @@ public class Tone_Setting extends JFrame{
     AudioFormat format;
     DataLine.Info info;
     Clip clip;
-    String path;
-
-
+    String path, tone;
+    File file;
 
     public Tone_Setting() {
+
+
         /*filePath = "F:/Coding/Avishkar2020/softablitz/project/TimeKeeper/src/audioFiles/alarm1.wav";
         audioInSt = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
         clip = AudioSystem.getClip();*/
 
-        chooseBT.addActionListener(new ActionListener() {
+
+        //-----------to choose from system---------////IMPORTANT/////DON'T REMOVE
+        /*chooseBT.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 FileDialog fD = new FileDialog(instance);
@@ -46,78 +49,57 @@ public class Tone_Setting extends JFrame{
                 fD.setVisible(true);
                 String audio = fD.getDirectory() + fD.getFile();
                 if (audio != null)
-                    file = audio;
+                    file = new File(audio);
                 try {
                     urlFile = new URL("file:" + file);
+                    //audioTnSt = AudioSystem.getAudioInputStream(new URL("file:" + file));
                 } catch (MalformedURLException ex) {
                     System.err.println(ex);
                 }
-            }
-        });
-
-        /*comboBox1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    urlFile = new URL("file:" + path);
-                } catch (MalformedURLException ex) {
-                    System.err.println(ex);
-                }
-                Applet.newAudioClip(urlFile).play();
-
-                try {
-                    audioTnSt = AudioSystem.getAudioInputStream(myFile);
-                } catch (UnsupportedAudioFileException unsupportedAudioFileException) {
-                    unsupportedAudioFileException.printStackTrace();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-                format = audioTnSt.getFormat();
-                info = new DataLine.Info(Clip.class, format);
-                try {
-                    clip = (Clip) AudioSystem.getLine(info);
-                } catch (LineUnavailableException lineUnavailableException) {
-                    lineUnavailableException.printStackTrace();
-                }
-                try {
-                    clip.open(audioTnSt);
-                } catch (LineUnavailableException lineUnavailableException) {
-                    lineUnavailableException.printStackTrace();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-                clip.start();
-
-
-                Object item = (Object) comboBox1.getSelectedItem();
-                try {
-                    item = audioInSt = AudioSystem.getAudioInputStream(myFile);
-                } catch (UnsupportedAudioFileException unsupportedAudioFileException) {
-                    unsupportedAudioFileException.printStackTrace();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-                format = audioInSt.getFormat();
-                info = new DataLine.Info(Clip.class, format);
-                clip = (Clip) AudioSystem.getLine(info);
-                setTone(item);
-                try {
-                    clip.open(audioInSt);
-                    clip.loop(Clip.LOOP_CONTINUOUSLY);
-                } catch (LineUnavailableException lineUnavailableException) {
-                    lineUnavailableException.printStackTrace();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-
             }
         });*/
+
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tone = comboBox1.getSelectedItem().toString();
+                if (tone.equals("Tone1")) {
+                    path = "F:\\Coding\\Avishkar2020\\softablitz\\project\\TimeKeeper\\src\\ToneSetting\\sounds\\alarm2.wav";
+                } else if (tone.equals("Tone2")) {
+                    path = "F:\\Coding\\Avishkar2020\\softablitz\\project\\TimeKeeper\\src\\ToneSetting\\sounds\\ringtone1.wav";
+                }
+                file = new File(path);
+                if(file.exists())
+                {
+                    try {
+                    fileWrite=new FileWriter("F:\\Coding\\Avishkar2020\\softablitz\\project\\TimeKeeper\\src\\ToneSetting\\toneSetting.txt");
+                    fileWrite.write(path);
+                    fileWrite.close();
+                     } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                    }
+
+                    try {
+                    audioTnSt = AudioSystem.getAudioInputStream(file);
+                    clip = AudioSystem.getClip();
+                    clip.open(audioTnSt);
+                    } catch (UnsupportedAudioFileException | LineUnavailableException | IOException unsupportedAudioFileException) {
+                    unsupportedAudioFileException.printStackTrace();
+                    }
+                } else
+                 {
+                    System.out.println("File doesn't exist");
+                 }
+            }
+        });
 
 
         playBT.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Applet.newAudioClip(urlFile).play();
+                //Applet.newAudioClip(urlFile).play();
+                clip.start();
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
                 System.out.println("playing");
             }
         });
@@ -125,7 +107,8 @@ public class Tone_Setting extends JFrame{
         stopBT.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Applet.newAudioClip(urlFile).stop();
+                //Applet.newAudioClip(urlFile).stop();
+                clip.stop();
                 System.out.println("stopped");
             }
         });
@@ -151,16 +134,16 @@ public class Tone_Setting extends JFrame{
     {
         if(tone.equals("Tone1"))
         {
-            filePath= "F:/Coding/Avishkar2020/softablitz/project/TimeKeeper/src/audioFiles/alarm1.wav";
+            path= "F:\\Coding\\Avishkar2020\\softablitz\\project\\TimeKeeper\\src\\ToneSetting\\sounds\\alarm1.wav";
         } else if (tone.equals("Tone2"))
         {
-            filePath= "F:/Coding/Avishkar2020/softablitz/project/TimeKeeper/src/audioFiles//audioFiles/alarm2.wav";
+            path= "F:\\Coding\\Avishkar2020\\softablitz\\project\\TimeKeeper\\src\\ToneSetting\\sounds\\alarm2.wav";
         } else if (tone.equals("Tone3"))
         {
-            filePath= "F:/Coding/Avishkar2020/softablitz/project/TimeKeeper/src/audioFiles//audioFiles/ringtone1.wav";
+            path= "F:\\Coding\\Avishkar2020\\softablitz\\project\\TimeKeeper\\src\\ToneSetting\\sounds\\ringtone1.wav";
         } else if (tone.equals("Tone4"))
         {
-            filePath= "F:/Coding/Avishkar2020/softablitz/project/TimeKeeper/src/audioFiles//audioFiles/ringtone2.wav";
+            path= "F:\\Coding\\Avishkar2020\\softablitz\\project\\TimeKeeper\\src\\ToneSetting\\sounds\\ringtone2.wav";
         }
 
     }*/
