5968ad893eb7f885b8f49e27af497a7ba4f605c0
Preeti Roj <preetiroj2001@gmail.com>
2020-10-25 00:10:07 +0530

    front window (Menu)

--- src/Manager/Menu.java
+++ src/Manager/Menu.java
@@ -1,7 +1,52 @@
 package Manager;
 
-public class Menu {
-    public void addVbox(){
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Stopwatch.*;
+
+public class Menu extends JPanel{
+    private JPanel jPanelMenu;
+    private JButton stopwatchButton;
+    private JButton timerButton;
+    private JButton timeZonesButton;
+    private JButton settingButton;
+    private JButton alarmButton;
+    TimeManager timeManager;
+
+
+
+    public Menu() {
+
+        FCalendar calendarObj = new FCalendar();
+        StandardClock standardClock= new StandardClock("Asia/Kolkata");
+        //tCD.setLayout(new GridLayout());
+        JFrame frame = new JFrame("Time Keeper");
+        //frame.setContentPane(new Menu().jPanelMenu);
+        frame.add(jPanelMenu);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(1000, 500);
+        frame.setLocation(200,100);
+        frame.add(calendarObj);
+        //frame.setLocation(270,100);
+        //frame.add(standardClock);
+        //frame.setLocation(500,100);
+        //frame.add(tCD);
+        frame.setVisible(true);
+
+        /*stopwatchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StopwatchUI stopwatchPanel = new StopwatchUI(timeManager);
+                frame.add(stopwatchPanel);
+            }
+        });*/
+    }
+
+    public static void main(String[] args) {
+        new Menu();
 
     }
 }
